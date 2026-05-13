b294228d18f52dd63473ca5c93d96e20473b0e75
michaelivanov <michivan2003@gmail.com>
2026-05-14 01:57:47 +0700

    perbaikan

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 import dotenv from 'dotenv'
+import ws from 'ws'
 dotenv.config()
 
 const supabaseUrl = process.env.SUPABASE_URL!
 const supabaseKey = process.env.SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  realtime: {
+    transport: ws
+  }
+})
